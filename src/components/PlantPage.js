a7e3971dd98d5b0188d049176f775883bba4a36f
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-02-08 13:09:58 -0800

    review solution

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((resp) => resp.json())
+      .then((allPlants) => setPlants(allPlants));
+  }, []);
+
+  function updateSearchTerm(search) {
+    setSearchTerm(search);
+  }
+
+  function addPlant(plant) {
+    const newPlants = [plant, ...plants];
+    setPlants(newPlants);
+  }
+
+  const filteredPlants = plants.filter((plant) => {
+    const lowerCaseName = plant.name.toLowerCase();
+    const lowerCaseSearch = searchTerm.toLowerCase();
+    const numPriceSearch = parseInt(searchTerm);
+    const isPrice = plant.price <= numPriceSearch;
+    return lowerCaseName.includes(lowerCaseSearch) || isPrice;
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search searchTerm={searchTerm} updateSearchTerm={updateSearchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
